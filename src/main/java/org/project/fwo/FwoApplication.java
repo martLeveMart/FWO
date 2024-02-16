b88782e610b6630f38c46d61b97681fe68b761a0
martLeveMart <mart.leveque69@gmail.com>
2024-02-17 00:16:18 +0100

    Create repo

---
+++ src/main/java/org/project/fwo/FwoApplication.java
@@ -0,0 +1,13 @@
+package org.project.fwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FwoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/project/fwo/FwoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.project.fwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

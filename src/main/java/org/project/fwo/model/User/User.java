9a4ab366388027d965d9acee1ad76ee03ef187e2
martLeveMart <mart.leveque69@gmail.com>
2024-02-17 01:25:29 +0100

    Correct sonar return

--- src/main/java/org/project/fwo/model/User/User.java
+++ src/main/java/org/project/fwo/model/User/User.java
@@ -1,4 +1,4 @@
-package model;
+package model.user;
 
 /*Déclaration des mise à jour
 History:
--- src/test/java/org/project/fwo/model/User/UserTest.java
+++ src/test/java/org/project/fwo/model/User/UserTest.java
@@ -1,4 +1,4 @@
-package model;
+package model.user;
 
 import net.bytebuddy.implementation.bind.annotation.RuntimeType;
 import org.junit.jupiter.api.Assertions;
